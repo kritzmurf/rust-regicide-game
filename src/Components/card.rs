cf2f3cca67ea06fce3237ffd1a82960545136bdc
Patrick <patmurf@live.com>
2024-08-13 19:48:34 -0500

    initial stub commit

---
+++ src/Components/card.rs
@@ -0,0 +1,27 @@
+enum Suit {
+    diamond,
+    heart,
+    spade,
+    club
+}
+
+enum Value {
+    ace,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    jack,
+    queen,
+    king
+}
+
+struct Card {
+    suit: Suit,
+
+}
